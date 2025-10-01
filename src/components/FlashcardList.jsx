68e3f8652bb61cc888f1cd352f8f0b492a5548d5
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-09-30 18:39:39 -0700

    Create component to contain all flashcards

---
+++ src/components/FlashcardList.jsx
@@ -0,0 +1,11 @@
+import Flashcard from "./Flashcard";
+
+const FlashcardList = () => {
+  return (
+    <div className="flashcard-list">
+      <Flashcard />
+    </div>
+  );
+};
+
+export default FlashcardList;
